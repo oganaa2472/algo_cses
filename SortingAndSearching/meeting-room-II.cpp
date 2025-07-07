3bbbc238724bde1bb680dc69a4435a96047d34e4
Gantogtokh <oganaa2472@gmail.com>
2025-07-07 18:16:06 -0500

    priority

---
+++ SortingAndSearching/meeting-room-II.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& a, auto& b) {
+            return a.start < b.start;
+        });
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (const auto& interval : intervals) {
+            if (!minHeap.empty() && minHeap.top() <= interval.start) {
+                minHeap.pop();
+            }
+            minHeap.push(interval.end);
+        }
+        return minHeap.size();
+    }
+};
