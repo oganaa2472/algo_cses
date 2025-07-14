1b843cf6b497691ad94faa1fee6c7c5f08abe406
Gantogtokh <oganaa2472@gmail.com>
2025-07-14 12:02:26 -0500

    add distnict_values

---
+++ SortingAndSearching/distnict_values.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main() {
+	// your code goes here
+	int n;
+    
+    cin>>n;
+    vector<ll> nums(n);
+    for (int i = 0; i < n; ++i) {
+        cin>>nums[i];
+    }
+    ll left = 0;
+    ll right = 0;
+    ll answer = 0;
+    set<ll> sets;
+    while(right<n){
+        while(sets.count(nums[right])>0){
+             sets.erase(nums[left]);
+             left++;
+        }
+        // cout<<right<<" "<<left<<" "<<endl;
+        // cout<<(right-left+1)<<endl;
+        answer = answer+(right-left+1);
+        sets.insert(nums[right]);
+        right++;
+    }
+    cout<<answer<<endl;
+}
