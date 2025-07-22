40fe7671f764be26e09b81f291d6689741d5bf64
Gantogtokh <oganaa2472@gmail.com>
2025-07-22 12:07:42 -0500

    add movie festiwal

--- SortingAndSearching/movie_II.cpp
+++ SortingAndSearching/movie_II.cpp
@@ -2,35 +2,32 @@
 using namespace std;
 
 int main() {
-    int n, k;
-    cin >> n >> k;
+	// your code goes here
+	
+	int n,k;
+	cin>>n>>k;
+	vector<pair<int,int>> events;
+	for(int i =0;i<n;i++){
+	   int a,b;
+	   cin>>a>>b;
+	   events.push_back({b,a});
+	}
+	sort(events.begin(),events.end());
+	multiset<int> times;
+	
+	for(int i = 1;i<=k;i++){
+	    times.insert(0);
+	}
+	
+	int answer =0;
+	for(auto [b,a]:events){
+	    auto it = times.upper_bound(a);
+	    if(it==times.begin()) continue;
+	    answer++;
+	    it--;
+	    times.erase(it);
+	    times.insert(b);
+	}
+	cout<<answer<<endl;
 
-    vector<pair<int, int>> movies(n);
-    for (int i = 0; i < n; i++) {
-        cin >> movies[i].first >> movies[i].second;
-    }
-
-    // Sort movies by ending time
-    sort(movies.begin(), movies.end(), [](auto &a, auto &b) {
-        return a.second < b.second;
-    });
-
-    // multiset of available times (when each member is free)
-    multiset<int> end_times;
-    for (int i = 0; i < k; i++) {
-        end_times.insert(0); // all members free at time 0
-    }
-
-    int count = 0;
-    for (auto [start, end] : movies) {
-        auto it = end_times.upper_bound(start);
-        if (it == end_times.begin()) continue; // no one available
-        --it; // member who is free at or before 'start'
-        end_times.erase(it);
-        end_times.insert(end);
-        count++;
-    }
-
-    cout << count << "\n";
-    return 0;
 }
